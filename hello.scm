f6e740beb50016fe3251fd6cee99d5d9ce9202f9
Michael Sampson <maniacalwhistle@gmail.com>
2024-10-03 00:04:42 -0400

    add some title text

--- hello.scm
+++ hello.scm
@@ -1,11 +1,13 @@
 (use-modules (chickadee graphics sprite))
 
-(define sprite (load-image "sprite-harp.png"))
+(define sprite (load-image "assets/sprite-harp.png"))
 
 (define position (vec2 0.0 240.0))
-
+(define title-position (vec2 250.0 400.0))
+(define title-color black)
 (define (draw alpha)
-  (draw-sprite sprite position))
+  (draw-sprite sprite position)
+  (draw-text "HARP ATTACK\n" title-position #:color title-color))
 
 (define (update dt)
   (update-agenda dt))
@@ -17,6 +19,7 @@
       (end 536.0)
       (duration 4.0))
   (script
-    (while #t
-           (tween duration start end update-x)
-           (tween duration end start update-x))))
+   (while #t
+     (tween duration start end update-x)
+     (tween duration end start update-x))))
+
